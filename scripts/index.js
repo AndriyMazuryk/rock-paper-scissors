fbb8558659697a7dd7a761f42d12e26952dd2fa9
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-03-04 20:37:10 +0200

    convert rules-window to an element

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
 import Game from "./game.js";
 
-const rulesButton = document.querySelector(".rules-button");
-const rulesWindow = document.querySelector("rules-window");
-rulesButton.addEventListener("click", () =>
-  rulesWindow.dispatchEvent(new CustomEvent("show"))
+const rulesBtn = document.querySelector(".rules-button");
+const rulesWindow = document.querySelector(".rules-window");
+const closeRulesBtn = document.querySelector(".rules-window__close-button");
+
+rulesBtn.addEventListener("click", () =>
+  rulesWindow.classList.add("rules-window--show")
+);
+closeRulesBtn.addEventListener("click", () =>
+  rulesWindow.classList.remove("rules-window--show")
 );
 
 const game = new Game();
-const score = parseInt(localStorage.getItem('score'));
+const score = parseInt(localStorage.getItem("score"));
 
 if (score) {
   game.init(score);
